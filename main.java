40d4905f9cf41f3a3f7c5f28f8008f3322c7d2a5
LukichaX <85228750+LukichaX@users.noreply.github.com>
2024-10-29 00:18:13 +0400

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ public class Main {
         try {
             Order order1 = new Order("ORD001", "Laptop", 1, 1200.50);
             Order order2 = new Order("ORD002", "Smartphone", 2, 800.00);
-            Order order3 = new Order("ORD003", "Tablet", 3, -150.00); // This will throw an exception
+            Order order3 = new Order("ORD003", "Tablet", 3, -150.00);
+
             System.out.println(order1);
             System.out.println(order2);
             System.out.println(order3);
